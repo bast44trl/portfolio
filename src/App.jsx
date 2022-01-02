5d5ec0c77c795903493538a445c8f188140e2694
Bastien Traille <b.traille@hotmail.fr>
2022-01-02 16:55:04 +0100

    added header and browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./app.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 import Homepage from "./components/Homepage/Homepage";
+import Tech from "./components/Tech/Tech";
+import AboutMe from "./components/AboutMe/AboutMe";
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <BrowserRouter basename="/">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/tech" element={<Tech />} />
+          <Route exact path="/aboutme" element={<AboutMe />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,14 +1,10 @@
-import AboutMe from '../AboutMe/AboutMe';
 import './homepage.scss';
 import LandingScreen from '../LandingScreen/LandingScreen';
-import Tech from '../Tech/Tech';
 
 const Homepage = () => {
     return (
       <div className='homepage'>
         <LandingScreen />
-        <Tech />
-        <AboutMe />
       </div>
     )
 }
