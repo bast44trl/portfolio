77c5dba2ee8d07570b7af061320fa52e97fd9259
Bastien Traille <b.traille@hotmail.fr>
2022-01-02 13:53:12 +0100

    started page tech

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,11 +1,13 @@
 import AboutMe from '../AboutMe/AboutMe';
 import './homepage.scss';
 import LandingScreen from '../LandingScreen/LandingScreen';
+import Tech from '../Tech/Tech';
 
 const Homepage = () => {
     return (
       <div className='homepage'>
         <LandingScreen />
+        <Tech />
         <AboutMe />
       </div>
     )
