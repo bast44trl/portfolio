5cafa60d6113c1ba3de86edc7d21506352659021
Bastien Traille <b.traille@hotmail.fr>
2022-01-19 15:18:35 +0100

    added update on top left

---
+++ src/components/Update/Update.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './update.scss';
+
+const Update = () => {
+    return (
+      <div className="update">
+        <p>{`Portfolio non terminé, merci pour votre indulgence 😊`}</p>
+        <p>{`(Desktop only, 1920 x 1080 px, le responsive arrive bientôt)`}</p>
+      </div>
+    );
+};
+
+export default Update;
